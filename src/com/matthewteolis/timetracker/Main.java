0be21b17cb32afa9786dec6efff8e8c95ac907cf
Matthew Teolis <matthewteolis@gmail.com>
2016-08-11 15:59:37 -0400

    Initial commit from old git account

---
+++ src/com/matthewteolis/timetracker/Main.java
@@ -0,0 +1,11 @@
+package com.matthewteolis.timetracker;
+
+import com.matthewteolis.timetracker.gui.MyFrame;
+
+public class Main
+{	
+	public static void main(String[] args)
+	{
+		MyFrame myFrame = new MyFrame();
+	}
+}
